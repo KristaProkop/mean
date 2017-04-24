c04f48e5f6eda69b0dd0b63b23f4e723cc2b46a1
kristaprokop <krista.prokop@gmail.com>
2017-04-24 11:10:06 -0500

    Survey form with socket emit

--- express/survey_one/server.js
+++ express/survey_one/server.js
@@ -1,34 +1,35 @@
 var express = require('express');
+
 var path = require('path');
+
 var app = express();
+
 var bodyParser = require('body-parser');
+
 
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use(express.static(path.join(__dirname, "./static")));
 
 app.set('views', path.join(__dirname, './views'));
+
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
     response.render('index');
 })
 
+app.post('/result', function (req, res) {
+    var survey_result = {
+        name: req.body['Name'], 
+        location: req.body['location'],
+        language: req.body['language'],
+        comment: req.body['comment'],
+    };
+    res.render('results', {result: survey_result});
+});
 
-var server = app.listen(8000, function() {
+
+app.listen(8000, function() {
     console.log('listening on port 8000');
 }); 
-
-var io = require('socket.io').listen(server);
-
-io.sockets.on('connection', function(socket) {
-    socket.on("data", function (inputs){
-        socket.emit('server_response', {
-            name: inputs.name,
-            location: inputs.location,
-            language: inputs.language,
-            comment: inputs.comment
-        });
-    });  
-})
-
